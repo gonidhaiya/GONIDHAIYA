d6ebe7fc2fc8993d1c6e2d0c8d4ed9aa4da06b22
MANU GONIDHAIYA <gonidhaiya@gmail.com>
2025-06-01 08:30:19 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        gold: {
+          500: "#FFD700",
+          600: "#FFC300"
+        },
+        green: {
+          950: "#013220"
+        }
+      }
+    }
+  },
+  plugins: [],
+};
